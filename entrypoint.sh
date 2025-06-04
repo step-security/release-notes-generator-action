4657f41d209332f0810fa0b996002507a0416587
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-06-04 14:21:18 +0530

    docker image push added

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ RESPONSE=$(curl --max-time 3 -s -w "%{http_code}" "$API_URL" -o /dev/null) || tr
 CURL_EXIT_CODE=${?}
 
 # Check if the response code is not 200
-if [ $CURL_EXIT_CODE -ne 0 ] || [ "$RESPONSE" != "200" ]; then
-  if [ -z "$RESPONSE" ] || [ "$RESPONSE" == "000" ] || [ $CURL_EXIT_CODE -ne 0 ]; then
+if [ "$CURL_EXIT_CODE" -ne 0 ] || [ "$RESPONSE" != "200" ]; then
+  if [ -z "$RESPONSE" ] || [ "$RESPONSE" = "000" ] || [ "$CURL_EXIT_CODE" -ne 0 ]; then
     echo "Timeout or API not reachable. Continuing to next step."
   else
     echo "Subscription is not valid. Reach out to support@stepsecurity.io"
@@ -33,26 +33,26 @@ echo "Action running with milestone $MILESTONE_ID_TO_USE on event $GITHUB_EVENT_
 
 #Check if Milestone exists, which means actions was raised by a milestone operation.
 if [ -z "$MILESTONE_ID_TO_USE" ]; then
-    echo "Milestone number is missing. Was the action raised by a milestone event?"
-    exit 1
+  echo "Milestone number is missing. Was the action raised by a milestone event?"
+  exit 1
 fi
 
 OUTPUT_FILENAME="release_file.md"
 
 #Check if we should use milestone title instead
 if [ -z "$FILENAME" ] && [ -n "$USE_MILESTONE_TITLE" ]; then
-    MILESTONE_TITLE=$(/JSON.sh < "${GITHUB_EVENT_PATH}" | grep '\["milestone","title"]' | cut -f2 | sed 's/\"//g' | sed 's/ /_/g')
-    OUTPUT_FILENAME="$MILESTONE_TITLE.md"
+  MILESTONE_TITLE=$(/JSON.sh < "${GITHUB_EVENT_PATH}" | grep '\["milestone","title"]' | cut -f2 | sed 's/\"//g' | sed 's/ /_/g')
+  OUTPUT_FILENAME="$MILESTONE_TITLE.md"
 fi
 
 #Check if a filename is provided
-if [ ! -z "$FILENAME" ]; then
-    OUTPUT_FILENAME="$FILENAME.md"
+if [ -n "$FILENAME" ]; then
+  OUTPUT_FILENAME="$FILENAME.md"
 fi
 
 #Check if a filename prefix is provided
-if [ ! -z "$FILENAME_PREFIX" ]; then
-  if [ ! -z "$FILENAME" ]; then
+if [ -n "$FILENAME_PREFIX" ]; then
+  if [ -n "$FILENAME" ]; then
     OUTPUT_FILENAME="$FILENAME_PREFIX$FILENAME.md"
   else
     OUTPUT_FILENAME="$FILENAME_PREFIX$MILESTONE_ID_TO_USE.md"
@@ -64,30 +64,30 @@ if [ -z "$OUTPUT_FOLDER" ]; then
   echo "OUTPUT_FOLDER ENV is missing, using the default one"
   OUTPUT_FOLDER='.'
 else
-  mkdir -p $OUTPUT_FOLDER
+  mkdir -p "$OUTPUT_FOLDER"
 fi
 
 echo "Checking for custom configuration..."
 CONFIG_FILE=".github/release-notes.yml"
-if [ ! -f ${CONFIG_FILE} ]; then
-    echo "No config file specified."
-    CONFIG_FILE=""
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "No config file specified."
+  CONFIG_FILE=""
 else
-    echo "Configuring the action using $CONFIG_FILE"
+  echo "Configuring the action using $CONFIG_FILE"
 fi
 
-if [ "workflow_dispatch" == "$GITHUB_EVENT_NAME" || "$ACTION" == "$TRIGGER_ACTION" ]; then
-    echo "Creating release notes for Milestone $MILESTONE_ID_TO_USE into the $OUTPUT_FILENAME file"
-    java -jar /github-release-notes-generator.jar \
-    --changelog.repository=${OWNER_ID}/${REPOSITORY_NAME} \
-    --github.username=${GH_USERNAME} \
-    --github.password=${GITHUB_TOKEN} \
+if [ "$GITHUB_EVENT_NAME" = "workflow_dispatch" ] || [ "$ACTION" = "$TRIGGER_ACTION" ]; then
+  echo "Creating release notes for Milestone $MILESTONE_ID_TO_USE into the $OUTPUT_FILENAME file"
+  java -jar /github-release-notes-generator.jar \
+    --changelog.repository="${OWNER_ID}/${REPOSITORY_NAME}" \
+    --github.username="${GH_USERNAME}" \
+    --github.password="${GITHUB_TOKEN}" \
     --changelog.milestone-reference=id \
-    --spring.config.location=${CONFIG_FILE} \
-    ${MILESTONE_ID_TO_USE} \
-    ${OUTPUT_FOLDER}/${OUTPUT_FILENAME}
-    cat ${OUTPUT_FOLDER}/${OUTPUT_FILENAME}
+    --spring.config.location="${CONFIG_FILE}" \
+    "${MILESTONE_ID_TO_USE}" \
+    "${OUTPUT_FOLDER}/${OUTPUT_FILENAME}"
+  cat "${OUTPUT_FOLDER}/${OUTPUT_FILENAME}"
 else
-    echo "Release notes generation skipped because action was: $ACTION"
-    exit 78
+  echo "Release notes generation skipped because action was: $ACTION"
+  exit 78
 fi
